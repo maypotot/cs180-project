00ba0be9b46beae2ccb6acee76357f9ec05622f9
maypotot <aapotot1@up.edu.ph>
2025-05-28 00:32:20 +0800

    final touches

--- cs180/src/routes/modelA/+page.svelte
+++ cs180/src/routes/modelA/+page.svelte
@@ -1,26 +1,45 @@
 <div class="flex justify-center p-12 space-y-4 max-w-10xl">
-    <div class="flex-col w-10/12 max-w-5xl text-center bg-gray-100 rounded-lg shadow-lg p-8">
+    <div class="flex flex-col w-10/12 justify-center items-center max-w-5xl text-center bg-gray-100 rounded-lg shadow-lg p-8">
         <h1 class="text-7xl font-bold mb-12">Model A</h1>
         <p class="font-mono text-5xl font-bold py-2 px-4">Description</p>
         <p class="font-mono text-lg py-2 px-4 text-justify">This is the first model of the project, which is a CNN that is based off the Resnet architecture that will be trained to classify the diseases or lack of in images of potato plants. </p>
         <p class="font-mono text-5xl font-bold py-2 px-4">Methodology</p>
 
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 1. Setting up the seed and the CPU/GPU dependencies</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">For reproducibility and make the GPU work for training</p>
-        
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 2. Extracting and Preprocessing Images</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">From the zip file, the dataset is extracted and cleaned based on the correct filetype, also deleting corruptfiles. The images are resized to 224 x 224 pixels and normalizing them to the image.net values. The Train Data was augmented to have verticalflip, horizontalflip, random rotation, color jitter, and gaussian blur to aviod overfitting and make the model not memorize the data. The validation dataset would not have an augmentation. We also used an 80-20 split train-validation dataset since test data set will be given later.</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 1. ZIP File extraction</p>
+        <p class="font-mono text-lg py-2 px-4 text-justify">Note that the ZIP file to be used must have the same level in the notebook's directory to ensure that this notebook runs smoothly. Otherwise, you may opt to download the zipfile from the Cloud and store it in the same folder as the notebook by uncommenting the GDrive commands in the succeeding code block below. ZIP File extraction is a must to extract the actual potato leaf disease development set.</p>
 
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 3. Building the Model</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">Our model uses the Resnet CNN model with pretrained = false for no weights will be used from online source, which is we will have a scratch resnet model. We replaced the head of the resnet to have a drop out of 50% to avoid overfitting. We also used AdamW optimizers, a scheduler that will reduce Learning rates on plateu and an criterion with entrophyloss.</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 2. Import essential libraries and filter corrupted images</p>
+        <img src="/pytorch.png" alt="pytorch" class="w-1/2 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">This model implementation relies on pytorch. As such, most imports were garnered from the said library. Without proper imports, the model will be unable to run properly. Apart from import, filtering invalid files will also ensure that the training and, effectively, the model will be produced as intended.</p>
 
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 4. Training the Model</p>        
-        <p class="font-mono text-lg py-2 px-4 text-justify">Our model trains for 50 epochs with a patience of 5 if there is no improvement from the validation accuracy. Also the train accuracy and validation accuracy is printed to check for overfitting and plateu for manual stopping</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 3. Set up the random seed</p>
+        <p class="font-mono text-lg py-2 px-4 text-justify">For this model, we set the random seed to 42 for reproducibility. Nevertheless, you may opt to configure the seed to a different value. Rest assured that the latter results, accuracy, and loss would still produce a model that is shielded against overfitting.</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 4. Set up proper CPU/GPU dependencies</p>
+        <img src="/gpu.png" alt="gpu" class="w-1/2 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">After numerous configurations on the model, training our model could breeze through a CPU dependency. Nevertheless, to ensure hardware safety, it would be best to use GPU (cuda) to safekeep hardware specs, and to enhance performance and training time.</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 5. Data augmentation, preprocessing, and data split</p>
+        <img src="/images_a.png" alt="images_a" class="w-1/2 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">To permit the images to properly react and classify images that are either distorted, illuminated, etc., data augmentation and preprocessing was applied before sending the blobs of images through training. The images are resized to 224x224 pixels and normalizing them to the image.net values. The train_data was augmented to have verticalflip, horizontalflip, random rotation, color jitter, and gaussian blur to avoid overfitting and make the model not memorize the data. The validation dataset would not have an augmentation. We also used an 80-20 split train-validation dataset since test data set will be given in succession.</p>
         
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 5. Evaluating the Model</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">Evaluating the Model through the precision, f1 scores, stability, recall, accuracy and specificity is printed at the end. Evaluation per class, we can see which classes this model classifies the best and which are not. We also have graph for the train vs validity values aswell as the confusion matrices, these finds whether there is overfitting and find the confusion between classes</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 6. Building the Model</p>
+        <img src="/resnet.png" alt="resnet" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Our model is built on a modified ResNet CNN model with pretrained set to false to ensure that the model will only be trained on the development set provided. This essentially sets the retrieved ResNet model to scratch or null. We replaced the head of the ResNet model to have a dropout of 50% (i.e., 0.5) to minimize overfitting. We also used Adam as the optimizer and a scheduler that will effectively reduce learning rates when accuracy begins to plateau. Learning rate reduction and losses are based on a criterion set with CrossEntropyLoss.</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 7. Training the Model</p>
+        <img src="/graph_a.png" alt="graph_a" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Our model trains for 50 epochs with a patience of 5 if there is no improvement from the validation accuracy. By setting the ResNet-based CNN to train, as epochs go by, we clear optimized parameters, compute loss, perform backpropagation, and update model parameters using computed gradients on the optimizer's algorithm (Adam).</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 8. Fine-tuning the model</p>        
+        <p class="font-mono text-lg py-2 px-4 text-justify">Fine-tuning the model uses different parameters in the optimizers. More specifically, with the Adam optimizer (initialized to "optimizer"), it gives different learning rates to the different layers of the model. This, in turn, allows the model to be fine-tuned with novel learnings (due to adjusted parameters) and gain an inccreased accuracy. This is achieved all while preventing overfitting, showing at least a 2% increase in accuracy (after fine-tuning).</p>
         
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 6. Predition</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">ALthough not included in this file, the demo_deeplearning_cnn has the notebook code to run and use the model to predict the test dataset given and gives a csv of the predictions per image..</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 9. Evaluating the Model</p>
+        <img src="/table_a.png" alt="table_a" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">To sum it all up, apart from the traing loss, validation loss, validation accuracy, and training accuracy, the model is evaluated through precision, f1 scores, stability, recall, accuracy and specificity metrics. Evaluations were done per class and from the produced results, we can see which classes this model classifies the best and which are not. A unifying graph for the train vs validation loss, train vs validatoin accuracy, classification report, and confusion matrix were revealed to show the wins and limitations that the model achieved after rigorous training.</p>
+        
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 10. Conclusion </p>
+        <img src="/confusion_a.png" alt="confusion_a" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Overall, our developed model for a deeplearning-based approach without the employment of transformer architectures was able to provide competitive and ample image classification to properly classify if potato leaves are diseased or not, based on the six delineated classes. More so, the produced model is able to generalize across different potato leaf disease outputs as revealed in the .csv file produced in the group9_demo1.ipynb demo notebook file. Despite the limited dataset, our model was able to get a satisfactory performance, showing a validation accuracy, precision, recall, f1-score, and specificity metric across all classes that lie at 62% as its base. From the evaluated confusion matrix, we could conclude that our model classifies Bacteria most confidently, but struggles the most with classifying Pest-ridden potato leaves. To sum it all up, by utilizing proper data augmentation, cleaning extraneous features, leveraging ResNet50's base model, and optimizing the ResNet CNN with head layer modifications, we were able to produce a competitive deeplearning-based model without transformer architecture employment, outside sources, or any pretrained model in a creative fashion.</p>
     </div>
 </div>
--- cs180/src/routes/modelB/+page.svelte
+++ cs180/src/routes/modelB/+page.svelte
@@ -1,24 +1,40 @@
 <div class="flex justify-center p-12 space-y-4 max-w-10xl">
-    <div class="flex-col w-10/12 max-w-5xl text-center bg-gray-100 rounded-lg shadow-lg p-8">
+    <div class="flex flex-col w-10/12 justify-center items-center max-w-5xl text-center bg-gray-100 rounded-lg shadow-lg p-8">
         <h1 class="text-7xl font-bold mb-12">Model B</h1>
         <p class="font-mono text-5xl font-bold py-2 px-4">Description</p>
         <p class="font-mono text-lg py-2 px-4 text-justify">This is the second model of the project, which is a deep learning model that employs a transfomer architecture that will be trained to classify the diseases or lack of in images of potato plants. </p>
         <p class="font-mono text-5xl font-bold py-2 px-4">Methodology</p>
 
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 1. Preprocessing the Images</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">Cleaning the dataset by resizing the images to 224 x 224 pixels and normalizing it to the Image.net values for normalization. For the Train Data, we used random Crop, random horizontalflip, random verticalflip, random rotation, color jitte, and gaussian blur to avoid overfitting and make the model memorize the data. For the validation data, no augmentations will be done. We used an 80 - 20 split for the train - validation, from the dataset, as the the test data will come at a later stage of this project.</p>
-        
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 2. Building the Model</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">Our model follows the traditional ViT model from the lessons, we build it from scratch. The model consists of a patch embedding layer instead of convolutional layer, where its purpose is to split the image into patches. We also have class token and position embedding which retain spatial information and handles the patch sequence. Next is the transformer layer which processes and learns the image. Lastly, the mlp Classification head handles the classification. The last two layers contain dropouts to handle overfitting and uses GELU.</p>
-        <img src="/vit.png" alt="ViT Model" class="w-full h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 1. Setup the Random Seed</p>
+        <p class="font-mono text-lg py-2 px-4 text-justify">We set the random seed to 42, which is for reproducibility. The values could be edited to a different value which would not affect the performance of the model and would not cause overfitting</p>
 
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 3. Training the Model</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">Training the model, we used 200 epochs since training ViT models from scratch needs alot of epochs to learn. ViT also needs alot of dataset, but currently we have very little which is why the 200 epoch and patience will be high. Per epoch, validity accuracy and loss, as well as train accuracy and loss is printed to check for overfitting and plateus for manual stopping.</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 2. Set up proper CPU/GPU dependencies</p>
+        <img src="/gpu.png" alt="gpu" class="w-1/2 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Using GPU is much better in terms of performance, training time, and it would safekeep hardware, while using CPU will be much more slower and also might fry your CPU, thus the need for GPU to be used</p>
 
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 4. Evaluating the Model</p>        
-        <p class="font-mono text-lg py-2 px-4 text-justify">Evaluatiung the model through accuracy, F1 scores, stabiility, recall and specificity per class is printed at the end of this notebook. With this evaluation we are able to evaluate the performance of the model per class, knowing which class has a harder time to recognize by the model. Evaluating also includes the graph for the Train vs Validity values aswell as the confusion matrices, which finds overfitting and find the models confusion between classes.</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 3. Preprocessing the Images</p>
+        <p class="font-mono text-lg py-2 px-4 text-justify">Cleaning the dataset by resizing the images to 288 x 288 pixels and normalizing it to the Image.net values for normalization. For the Train Data, we used random Crop, random horizontalflip, random verticalflip, random rotation, color jitter, and gaussian blur to avoid overfitting and make the model not memorize the data. For the validation data, no augmentations will be done.
+We used an 80 - 20 split for the train - validation, from the dataset, as the the test data will come at a later stage of this project.</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 4. Building the Model</p>
+        <img src="/vit.png" alt="healthy" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Our model follows the traditional ViT model from the lessons, we build it from scratch. The model consists of a patch embedding layer instead of convolutional layer, where its purpose is to split the image into patches. We also have class token and position embedding which retain spatial information and handles the patch sequence. Next is the transformer layer which processes and learns the image. Lastly, the mlp Classification head handles the classification. The last two layers contain dropouts to handle overfitting and uses GELU</p>
+
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 5. Training the Model</p>
+        <img src="/training_b.png" alt="training_b" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Training the model, we used 200 epochs since training ViT models from scratch needs alot of epochs to learn. ViT also needs alot of dataset, but currently we have very little which is why the 200 epoch and patience will be high. Per epoch, validity accuracy and loss, as well as train accuracy and loss is printed to check for overfitting and plateus for manual stopping</p>
         
-        <p class="font-mono text-4xl font-bold py-2 px-4">Step 5. Preditcion</p>
-        <p class="font-mono text-lg py-2 px-4 text-justify">After, using the test dataset, the model is used to predict this dataset, which is saved at the deeplearning_vit_predictions.csv file, with also evaluation on how it accuractely classifies potato diseases</p>
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 6. Evaluating the Model</p>
+        <img src="/table_b.png" alt="training_b" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Evaluatiung the model through accuracy, F1 scores, stability, recall and specificity per class is printed at the end of this notebook. With this evaluation we are able to evaluate the performance of the model per class, knowing which class has a harder time to recognize by the model. Evaluating also includes the graph for the Train vs Validity values aswell as the confusion matrices, which finds overfitting and find the models confusion between classes.</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 7. Prediction using Test Dataset</p>
+        <img src="/csv.png" alt="csv" class="w-1/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">After, using the test dataset, the model is used to predict this dataset, which is saved at the deeplearning_vit_predictions.csv file, with also evaluation on how it accuractely classifies potato diseases.</p>
+
+        <p class="font-mono text-4xl font-bold py-2 px-4">Step 8. Conclusion </p>
+        <img src="/confusion_b.png" alt="confusion_b" class="w-3/4 h-auto my-4 rounded-lg shadow-lg">
+        <p class="font-mono text-lg py-2 px-4 text-justify">Overall, our developed deeplearning-based approach using the Vision Transformer was able to also provide an competitive image classification model for classifying potato diseases with a validation accuracy of 61%, which also generalizes majority of potato leaf diseases. With using patching layer to patch the images and train them per patch and learning that with our ViT model, following the architechture of the traditional ViT helped us create this model achieving satisfactory results. As read from sources, ViT requires lots of datasets with 10k above as its minimum, having 4k datasets is a huge blow to the performance of our model as it reaches a stagnant accuracy at epoch 80s above. Also ViT requires lots of epochs, but with the lack of datasets it plateu at 80 epoch in which the normal ViT with 10k images reaaches to 200 and more. With our model achieving 62% accuracy from the testing, in the Classification Matrix, the precision has a 67%, and a 67% F1-Score, with that this model have achieved a satisfactory model for classifying Potato Disease from its leaves. The confusion matrix shows that the model is having troubled differentiating pest and fungi, and having the best in classifying fungi. One thing that we can improve here is in our model is maybe finding another way to get a lot of datasets to feed in our ViT, also we havent explored much using pretrained models to be added or finetuned in this model. This is our creative model especially with guide from the ViT architecture, and tweaking the layers, we are able to create this satisfactory model.</p>
     </div>
 </div>
